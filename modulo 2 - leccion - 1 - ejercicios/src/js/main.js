d9bb3553a750026f36d704d74a21a134c4f0e04a
Rosa Donoso <rosa.donoso.requena@outlook.de>
2021-11-24 15:03:59 +0100

    started Modulo 2

---
+++ modulo 2 - leccion - 1 - ejercicios/src/js/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+
+
+const titleElement = document.querySelector ('.titulo');
+const listElement = document.querySelector ('.loser');
+
+titleElement.innerHTML = titleElement.innerHTML + listElement.innerHTML;
+
