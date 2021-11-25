61ac00995f4424dbe2ecc505eb22fcc4f66e3534
Rosa Donoso <rosa.donoso.requena@outlook.de>
2021-11-25 09:57:35 +0100

    added new javascript exercises

--- modulo 2 - leccion - 1 - ejercicios/src/js/main.js
+++ modulo 2 - leccion - 1 - ejercicios/src/js/main.js
@@ -1,9 +1,32 @@
 'use strict';
+//const titleElement = document.querySelector ('.titulo');
+//const listElement = document.querySelector ('.loser');
+//const navElement = document.querySelector ('.list');
+//const content = '<li><a href="#">Home</a></li><li><a href="#">Contact</a></li>';
+//const password = document.querySelector ('.password');
+//const divElement = document.querySelector ('.container');
+//const enunElement = document.querySelector ('.enunciado');
+//const imgElement = document.querySelector ('.picture');
+//const contentElement = document.querySelector ('.content');
+//const Titu = 'Lorem ipsum';
+//const Foto = 'https://via.placeholder.com/350x150';
+//const pe = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit';
+//const EjercicioElement = document.querySelector ('.test');
+//const contenido = '<li>1</li><li>2</li><li>3</li>';
+//EjercicioElement.innerHTML = contenido;
 
 
 
-const titleElement = document.querySelector ('.titulo');
-const listElement = document.querySelector ('.loser');
+//titleElement.innerHTML = titleElement.innerHTML + listElement.innerHTML;
+//navElement.innerHTML = content;
+//password.innerHTML = ('******');
+//divElement.innerHTML = Titu, Foto, pe; ¿Se pueden meter varias constantes en un mismo elemento?
 
-titleElement.innerHTML = titleElement.innerHTML + listElement.innerHTML;
+
+//enunElement.innerHTML = Titu;
+//imgElement.innerHTML = Foto;
+//contentElement.innerHTML = pe;
+
+
+
 
