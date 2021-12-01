94e136259e8e82c9e58485158a735abfeafb2494
Rosa Donoso <rosa.donoso.requena@outlook.de>
2021-12-01 08:52:57 +0100

    exercises

--- modulo 2 - leccion - 2 - ejercicios/src/js/main.js
+++ modulo 2 - leccion - 2 - ejercicios/src/js/main.js
@@ -1,3 +1,7 @@
 'use strict';
+const userAge = document.querySelector('.js-age');
+const LifeHours = 8760 * parseInt(userAge.innerHTML);
 
-console.log('>> Ready :)');
+document.querySelector('h1').innerHTML = (`He vivido ${LifeHours} horas`);
+
+
