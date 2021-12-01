f201acabb80c1e71070a8f38a2df9dfd04443a62
Rosa Donoso <rosa.donoso.requena@outlook.de>
2021-12-01 21:28:12 +0100

    added exercise

--- modulo - 2 - leccion 3 - ejercicios/src/js/main.js
+++ modulo - 2 - leccion 3 - ejercicios/src/js/main.js
@@ -5,7 +5,7 @@ const DEFAULT_AVATAR = 'http://placehold.it/300x300';
 const usuario = document.querySelector('.js-user');
 // avatar que eligió el usuario al registrarse
 let userAvatar = 'http://www.fillmurray.com/300/300';
-let html =   `<img class="user__avatar js-user__avatar ${userAvatar} />;`
+document.querySelector('img').innerHTML = 'http://www.fillmurray.com/300/300';
 
 
 
