14e946721304271409ffdb9ff392c1d99489b6ac
Rosa Donoso <rosa.donoso.requena@outlook.de>
2021-12-04 09:04:14 +0100

    added exercise 2

---
+++ Modulo 2 - leccion - 3 - ejercicios/src/js/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+
+/*// avatar por defecto
+const DEFAULT_AVATAR = 'http://placehold.it/300x300';
+// avatar que eligió el usuario al registrarse
+let userAvatar = 'http://www.fillmurray.com/300/300';
+const userImage = document.querySelector('.js-user__avatar');
+userImage.src = `${userAvatar || DEFAULT_AVATAR}`;
+
+
+userImage.src = (userAvatar === "") ? DEFAULT_AVATAR : userAvatar;*/
+
+//ejercicio 2
+let myName = 'Rosa';
+let yourName = 'Ana';
