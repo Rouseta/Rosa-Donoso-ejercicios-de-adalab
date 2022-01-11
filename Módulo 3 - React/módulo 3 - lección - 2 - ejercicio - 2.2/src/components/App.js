39bb24c794f01cc8db7bf166b62604266f86cb30
Rosa Donoso <rosa.donoso.requena@outlook.de>
2022-01-11 21:32:47 +0100

    added exercise counter

--- Módulo 3 - React/módulo 3 - lección - 2 - ejercicio - 2.2/src/components/App.js
+++ Módulo 3 - React/módulo 3 - lección - 2 - ejercicio - 2.2/src/components/App.js
@@ -1,15 +1,41 @@
-import "../styles/App.scss";
 
+
+// Fichero src/components/App.js
+// Importamos useState desde React porque lo vamos a necesitar más abajo
+import { useState } from 'react';
+
+// Creamos la función o componente App. Esta vez lo creamos con una función arrow.
 const App = () => {
-  const handleButton = (ev) => {
-    console.log('El botón ha sido pulsado');
-    console.log('El evento lanzado es: ', ev);
-  };
+  const [numberOfClicks, setNumberOfClicks] = useState(0);
+  const handleClick = () => {
+    setNumberOfClicks(numberOfClicks + 1);
+
+  }
+
+
+
+
   return (
     <div>
-      <button onClick={handleButton}>Púlsame</button>
-    </div>
-  );
+      {/* <h1>Contador: {numberOfClicks}</h1>
+
+      <button onClick={() => setNumberOfClicks(numberOfClicks + 1)}>click
+      </button> */}
+      <h1>Contador: el número de clicks es de {numberOfClicks}</h1>
+      <button onClick={handleClick}>click y suma</button>
+
+
+
+    </div >
+  )
+
 }
+
+
+
+
+// Retornamos todo el código HTML que queremos que React pinte en la página.
+
+
 
 export default App;
---
+++ Módulo 3 - React/módulo 3 - lección - 2 - ejercicio - 2.4/src/components/App.js
@@ -0,0 +1,11 @@
+
+import '../styles/App.scss';
+
+function App() {
+  return (
+    <div><h1>¡Hola Mundo!</h1></div>
+
+  );
+}
+
+export default App;
