572c8e4ca4a11462957876db853f6ceb96d48247
Rosa Donoso <rosa.donoso.requena@outlook.de>
2022-01-11 22:09:31 +0100

    added exercise dark Mode

--- Módulo 3 - React/módulo 3 - lección - 2 - ejercicio - 2.4/src/components/App.js
+++ Módulo 3 - React/módulo 3 - lección - 2 - ejercicio - 2.4/src/components/App.js
@@ -1,10 +1,41 @@
-
-import '../styles/App.scss';
+import { useState } from "react";
+import "../styles/App.scss";
 
 function App() {
-  return (
-    <div><h1>¡Hola Mundo!</h1></div>
+  const [darkModeOn, setDarkModeOn] = useState(true);
 
+  const handleDarkMode = () => {
+    console.log("click");
+    setDarkModeOn(!darkModeOn);
+  };
+  const renderParagraph = () => {
+    if (darkModeOn === true) {
+      return (
+        <div>
+          <p className="paragraph ">Tienes activado el dark mode</p>
+          <p className="paragraph darkMode">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+            modi expedita, minus, quas nesciunt eius optio facilis tenetur
+            provident beatae, corporis non voluptas. Fugit nisi ex provident
+            quasi vel eligendi.
+          </p>
+        </div>
+      );
+    } else {
+      return null;
+    }
+  };
+
+  return (
+    <div>
+      <div>
+        <button className="onOff" onClick={handleDarkMode}>
+          Activar/Desactivar Dark Mode
+        </button>
+        {renderParagraph()}
+        {/* <p className="paragraph">Tienes activado el dark mode</p> */}
+      </div>
+    </div>
   );
 }
 
